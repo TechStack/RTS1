e4a75cba92dfa2b5d7114cb1d103e14ae8f6c7be
TechStack <techStackLp@gmail.com>
2016-03-13 16:40:09 -0400

    added Mech

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+
+  public  Transform target;
+    public int rotationSpeed;
+    public Rigidbody rigidBody;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(target.position - transform.position), rotationSpeed * Time.deltaTime)           ;
+
+        rigidBody.AddForce(transform.forward, ForceMode.Impulse);
+
+        //transform.position += transform.forward * 2 * Time.deltaTime;
+
+    }
+}
